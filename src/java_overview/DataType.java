2be354c4e63b785b715538ed88a57aa233148a25
Hao <hao2001.dev@gmail.com>
2024-06-24 11:28:39 +0700

    first commit

---
+++ src/java_overview/DataType.java
@@ -0,0 +1,12 @@
+package java_overview;
+
+import java.util.Scanner;
+
+public class DataType {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+  }
+}
---
+++ src/java_overview/HelloWorld.java
@@ -0,0 +1,8 @@
+package java_overview;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
